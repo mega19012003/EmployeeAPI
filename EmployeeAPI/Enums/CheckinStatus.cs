abd7ab9d4db397ffbd743910098f85399ff99de9
tritranduc <mega19012003@gmail.com>
2025-05-30 13:55:48 +0700

    sủa lại num checkin status

--- EmployeeAPI/Enums/CheckinStatus.cs
+++ EmployeeAPI/Enums/CheckinStatus.cs
@@ -2,12 +2,18 @@
 {
     public enum CheckinStatus
     {
+        //OnTime = 0,
+        //Late = 1,
+        //LeaveEarly = 2,
+        //Overtime = 3,
+        //Absent = 4,
+        //LeaveWithPermission = 5,
+        //Others = 6,
         OnTime = 0,
         Late = 1,
-        LeaveEarly = 2,
-        Overtime = 3,
-        Absent = 4,
-        LeaveWithPermission = 5,
-        Others = 6,
+        Overtime = 2,
+        Absent = 3,
+        LeaveWithPermission = 4,
+        Others = 5,
     }
 }
