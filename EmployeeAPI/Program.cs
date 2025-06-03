a0c2dd75152f3152a46a1e02b587e284407f096b
tritranduc <mega19012003@gmail.com>
2025-06-03 21:52:03 +0700

    asd

--- EmployeeAPI/Program.cs
+++ EmployeeAPI/Program.cs
@@ -135,7 +135,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 Encoding.UTF8.GetBytes(jwtSetting.Key))
         };
     });
-
+//
 builder.Services.AddControllers();
 //Ý nghĩa: Đăng ký dịch vụ Controllers vào Dependency Injection (DI container).
 //Tác dụng: Cho phép bạn sử dụng các Controller (thường nằm trong thư mục Controllers/) để xử lý các HTTP request (GET, POST, PUT, DELETE...).
