a0243213a084ff4f7fd8ffef5b89edd750705620
Tritran <mega19012003@gmail.com>
2025-07-09 17:29:03 +0700

    cicd

--- EmployeeAPI/Models/ScheduleTime.cs
+++ EmployeeAPI/Models/ScheduleTime.cs
@@ -12,7 +12,7 @@ namespace EmployeeAPI.Models
         public TimeOnly StartTimeMorning { get; set; } 
         public TimeOnly EndTimeMorning { get; set; }
         public int LogAllowtime { get; set; }
-        //public int LateThresholdMinutes { get; set; } 
+         //public int LateThresholdMinutes { get; set; } 
         public TimeOnly StartTimeAfternoon { get; set; }
         public TimeOnly EndTimeAfternoon { get; set; } 
     }
