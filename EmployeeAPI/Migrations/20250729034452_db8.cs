ce794f58c7f4a8d18a5b52cb19f9a387d9f10a99
Tritran <mega19012003@gmail.com>
2025-07-29 10:49:45 +0700

    cicd

---
+++ EmployeeAPI/Migrations/20250729034452_db8.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class db8 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isPaid",
+                table: "Payrolls");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isPaid",
+                table: "Payrolls",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- EmployeeAPI/Models/Payroll.cs
+++ EmployeeAPI/Models/Payroll.cs
@@ -14,6 +14,5 @@ namespace EmployeeAPI.Models
         public string Note { get; set; }
         public bool IsDeleted { get; set; } = false;
         public int DaysWorked { get; set; } 
-        public bool isPaid { get; set; }
     }
 }
